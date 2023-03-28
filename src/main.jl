acac37153c1fe616f2f9a0e12080f21f90617afd
Joe S <schnetzlerjoe@gmail.com>
2023-03-27 23:39:47 -0500

    Add mongo and cli

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,29 @@
 include("price.jl")
 include("speed.jl")
 include("validator.jl")
+using Comonicon
 
-function main()
+@cast mycmd1(arg; option="Sam") = println("cmd1: arg=", arg, "option=", option)
+@cast mycmd2(arg; option="Sam") = println("cmd2: arg=", arg, "option=", option)
+
+"""
+a module
+"""
+module Cmd3
+
+using Comonicon
+
+@cast mycmd4(arg) = println("cmd4: arg=", arg)
+
+end # module
+
+@cast Cmd3
+
+"""
+Defund Indexer CLI
+"""
+@main function start(arg; option="Sam", flag::Bool=false)
+    @show arg
+    @show option
+    @show flag
 end
---
+++ src/mongo.jl
@@ -0,0 +1,11 @@
+using Mongoc
+
+client = Mongoc.Client(ENV["MONGO_URL"])
+database = client["orbit-alpha-1"]
+
+function addDoc(collection_name::String, doc::Dict)
+    collection  = database[collection_name]
+    document = Mongoc.BSON(doc)
+    result = push!(collection, document)
+    return result
+end
